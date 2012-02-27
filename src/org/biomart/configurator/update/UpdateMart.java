c6bc59e3c787664733e05c9e5e0f0eea140304f3
Yong Liang <yong.liang@oicr.on.ca>
2012-02-27 15:55:08 +0000

    disable orphan column check
    git-svn-id: https://code.oicr.on.ca/svn/biomart/biomart-java/branches/release-0_8-candidate_7@11619 8dd1c8d6-c59f-4bb3-b62a-300035dbf9e7

--- src/org/biomart/configurator/update/UpdateMart.java
+++ src/org/biomart/configurator/update/UpdateMart.java
@@ -1,17 +1,14 @@
 package org.biomart.configurator.update;
 
 import java.util.List;
-
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
-
 import org.biomart.common.exceptions.MartBuilderException;
 import org.biomart.common.resources.Resources;
 import org.biomart.common.view.gui.dialogs.ProgressDialog;
 import org.biomart.common.view.gui.dialogs.StackTrace;
-import org.biomart.configurator.controller.MartController;
 import org.biomart.configurator.model.object.DataLinkInfo;
 import org.biomart.configurator.utils.type.DataLinkType;
 import org.biomart.objects.objects.Dataset;
@@ -19,93 +16,91 @@ import org.biomart.objects.objects.Mart;
 
 public class UpdateMart {
 
-	private ProgressDialog progressMonitor;
-	private UpdateMartModel updateModel;
-	public UpdateMartModel getUpdateModel() {
-		return updateModel;
-	}
+    private ProgressDialog progressMonitor;
+    private UpdateMartModel updateModel;
 
-	/*
-	 * the dataset may from db (0.7/0.8), url (0.7/0.8), need to handle all the cases
-	 */
-	public UpdateMart(JDialog parent, ProgressDialog pd){
-		if(pd == null){
-			this.progressMonitor = ProgressDialog.getInstance(parent);
-		}else{
-			this.progressMonitor = pd;
-		}
-		this.updateModel = new UpdateMartModel();
-	}
-	
-	public boolean updateDataset(List<Dataset> dslist, boolean updateOption) throws MartBuilderException {
-		for(Dataset ds : dslist){
-			this.progressMonitor.setStatus("updating dataset: "+ds.getDisplayName());
-			this.updateModel.updateDataset(ds, updateOption);
-		}
-		Mart mart = dslist.get(0).getParentMart();
-		if(updateOption)
-			this.updateModel.updateOptions(mart, dslist);
-		return true;
-	}
-	
-	public void updateMartWithProgressBar(final Mart mart) {
-		
-		final SwingWorker<Void,Void> worker = new SwingWorker<Void,Void>() {
+    public UpdateMartModel getUpdateModel() {
+        return updateModel;
+    }
 
-			@Override
-			protected Void doInBackground() throws Exception {
-				try {
-					progressMonitor.setStatus("updating mart ...");
-					updateModel.updateMart(mart);
-					//update options
-					int n = JOptionPane.showConfirmDialog(
-						    null,
-						    "update options in mart "+mart.getName()+" ?",
-						    "Question",
-						    JOptionPane.YES_NO_OPTION);
+    /*
+     * the dataset may from db (0.7/0.8), url (0.7/0.8), need to handle all the cases
+     */
+    public UpdateMart(JDialog parent, ProgressDialog pd) {
+        if (pd == null) {
+            this.progressMonitor = ProgressDialog.getInstance(parent);
+        } else {
+            this.progressMonitor = pd;
+        }
+        this.updateModel = new UpdateMartModel();
+    }
 
-					if(n==0) {
-						progressMonitor.setStatus("updating options from mart "+mart.getName());
-						updateModel.updateOptions(mart,mart.getDatasetList());
-					}	
-					//add naive attribute/filter for orphan column?
-					boolean checkOrphanColumns = false;
-					for(Dataset ds: mart.getDatasetList()) {
-						//check if it is hidden for the master
-						DataLinkInfo dlink = ds.getDataLinkInfoNonFlip();
-						if(dlink.getDataLinkType() == DataLinkType.SOURCE || dlink.getDataLinkType() == DataLinkType.TARGET) {
-							if(ds.hideOnConfig(mart.getMasterConfig()))
-								continue;
-							checkOrphanColumns = true;
-							break;
-						}
-					}
-					if(checkOrphanColumns) {
-						progressMonitor.setStatus("checking orphan column");
-						MartController.getInstance().createNaiveForOrphanColumn(mart);
-					}
-				} catch (final Throwable t) {
-					SwingUtilities.invokeLater(new Runnable() {
-						public void run() {
-							StackTrace.showStackTrace(t);
-						}
-					});
-				}finally {
-					progressMonitor.setVisible(false);
-				}
-				return null;
-			}
-			
-			@Override
-			protected void done() {
-				progressMonitor.setVisible(false);	
-			}
-		};
-		
-		worker.execute();
-		progressMonitor.start(Resources.get("DEFAULTPRECESSMESSAGE"));
+    public boolean updateDataset(List<Dataset> dslist, boolean updateOption) throws MartBuilderException {
+        for (Dataset ds : dslist) {
+            this.progressMonitor.setStatus("updating dataset: " + ds.getDisplayName());
+            this.updateModel.updateDataset(ds, updateOption);
+        }
+        Mart mart = dslist.get(0).getParentMart();
+        if (updateOption)
+            this.updateModel.updateOptions(mart, dslist);
+        return true;
+    }
 
-	}
-	
+    public void updateMartWithProgressBar(final Mart mart) {
+
+        final SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+
+            @Override
+            protected Void doInBackground() throws Exception {
+                try {
+                    progressMonitor.setStatus("updating mart ...");
+                    updateModel.updateMart(mart);
+                    // update options
+                    int n = JOptionPane.showConfirmDialog(null, "update options in mart " + mart.getName() + " ?",
+                            "Question", JOptionPane.YES_NO_OPTION);
+
+                    if (n == 0) {
+                        progressMonitor.setStatus("updating options from mart " + mart.getName());
+                        updateModel.updateOptions(mart, mart.getDatasetList());
+                    }
+                    // add naive attribute/filter for orphan column?
+                    boolean checkOrphanColumns = false;
+                    for (Dataset ds : mart.getDatasetList()) {
+                        // check if it is hidden for the master
+                        DataLinkInfo dlink = ds.getDataLinkInfoNonFlip();
+                        if (dlink.getDataLinkType() == DataLinkType.SOURCE
+                                || dlink.getDataLinkType() == DataLinkType.TARGET) {
+                            if (ds.hideOnConfig(mart.getMasterConfig()))
+                                continue;
+                            checkOrphanColumns = true;
+                            break;
+                        }
+                    }
+                    /*
+                     * if(checkOrphanColumns) { progressMonitor.setStatus("checking orphan column");
+                     * MartController.getInstance().createNaiveForOrphanColumn(mart); }
+                     */
+                } catch (final Throwable t) {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            StackTrace.showStackTrace(t);
+                        }
+                    });
+                } finally {
+                    progressMonitor.setVisible(false);
+                }
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                progressMonitor.setVisible(false);
+            }
+        };
+
+        worker.execute();
+        progressMonitor.start(Resources.get("DEFAULTPRECESSMESSAGE"));
+
+    }
 
 }
