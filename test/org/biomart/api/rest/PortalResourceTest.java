6c291817fa5a435d74bf442c0b3c5f6990602e29
Yong Liang <yong.liang@oicr.on.ca>
2012-02-28 20:42:37 +0000

    test
    git-svn-id: https://code.oicr.on.ca/svn/biomart/biomart-java/branches/release-0_8-candidate_7@11634 8dd1c8d6-c59f-4bb3-b62a-300035dbf9e7

--- test/org/biomart/api/rest/PortalResourceTest.java
+++ test/org/biomart/api/rest/PortalResourceTest.java
@@ -1,19 +1,22 @@
 package org.biomart.api.rest;
 
-import org.biomart.web.TestServletConfig;
-import com.google.inject.servlet.GuiceFilter;
-import com.sun.jersey.test.framework.WebAppDescriptor;
-import java.util.Iterator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import java.io.IOException;
+import java.util.Iterator;
+import org.biomart.web.TestServletConfig;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.jfree.util.Log;
+import org.junit.Test;
+import com.google.inject.servlet.GuiceFilter;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.JerseyTest;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import com.sun.jersey.test.framework.WebAppDescriptor;
 
 /**
- *
+ * 
  * @author jhsu
  */
 public class PortalResourceTest extends JerseyTest {
@@ -25,13 +28,8 @@ public class PortalResourceTest extends JerseyTest {
     }
 
     public PortalResourceTest() throws Exception {
-        super(new WebAppDescriptor.Builder()
-                .contextListenerClass(TestServletConfig.class)
-                .filterClass(GuiceFilter.class)
-                .contextPath("/")
-                .servletPath("/")
-                .build()
-        );
+        super(new WebAppDescriptor.Builder().contextListenerClass(TestServletConfig.class)
+                .filterClass(GuiceFilter.class).contextPath("/").servletPath("/").build());
     }
 
     private boolean assertPropertiesNotNull(JsonNode node, String... args) {
@@ -47,34 +45,28 @@ public class PortalResourceTest extends JerseyTest {
     @Test
     public void testGetMartList() throws IOException {
         WebResource webResource = resource();
-        String responseMsg = webResource
-                .path("/martservice/marts")
-                .accept("application/json")
-                .get(String.class);
+        String responseMsg = webResource.path("/martservice/marts").accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
-            assertPropertiesNotNull(curr, "displayName", "name",
-                    "config", "operation", "meta");
+            assertPropertiesNotNull(curr, "displayName", "name", "config", "operation", "meta");
         }
     }
 
     @Test
     public void testGetDatasetList() throws IOException {
         WebResource webResource = resource();
-        String responseMsg = webResource.path("/martservice/datasets")
-                .queryParam("mart", "gene")
-                .accept("application/json")
-                .get(String.class);
+        String responseMsg = webResource.path("/martservice/datasets").queryParam("mart", "gene")
+                .accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
             assertPropertiesNotNull(curr, "displayName", "name", "isHidden", "description");
         }
@@ -84,16 +76,14 @@ public class PortalResourceTest extends JerseyTest {
     public void testGetAllFilters() throws IOException {
         WebResource webResource = resource();
         String responseMsg = webResource.path("/martservice/filters")
-                .queryParam("datasets", "homo_sapiens_core_60_37e")
-                .queryParam("config", "gene")
-                .accept("application/json")
-                .get(String.class);
+                .queryParam("datasets", "homo_sapiens_core_60_37e").queryParam("config", "gene")
+                .accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
             assertPropertiesNotNull(curr, "displayName", "name", "isHidden", "type", "description");
         }
@@ -103,28 +93,23 @@ public class PortalResourceTest extends JerseyTest {
     public void testGetAllAttributes() throws IOException {
         WebResource webResource = resource();
         String responseMsg = webResource.path("/martservice/attributes")
-                .queryParam("datasets", "homo_sapiens_core_60_37e")
-                .queryParam("config", "gene")
-                .accept("application/json")
-                .get(String.class);
+                .queryParam("datasets", "homo_sapiens_core_60_37e").queryParam("config", "gene")
+                .accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
-            assertPropertiesNotNull(curr, "displayName", "name", "isHidden",
-                    "value", "linkURL");
+            assertPropertiesNotNull(curr, "displayName", "name", "isHidden", "value", "linkURL");
         }
     }
 
     @Test
     public void testGetRootGuiContainer() throws IOException {
         WebResource webResource = resource();
-        String responseMsg = webResource.path("/martservice/portal")
-                .accept("application/json")
-                .get(String.class);
+        String responseMsg = webResource.path("/martservice/portal").accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
 
@@ -134,7 +119,7 @@ public class PortalResourceTest extends JerseyTest {
 
         assertTrue(children.isArray());
 
-        for(Iterator<JsonNode> it = children.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = children.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
             assertPropertiesNotNull(curr, "displayName", "name", "guiType");
             assertTrue(curr.get("marts") != null || curr.get("guiContainers") != null);
@@ -145,10 +130,8 @@ public class PortalResourceTest extends JerseyTest {
     public void testGetRootContainer() throws IOException {
         WebResource webResource = resource();
         String responseMsg = webResource.path("/martservice/containers")
-                .queryParam("datasets", "homo_sapiens_core_60_37e")
-                .queryParam("config", "gene")
-                .accept("application/json")
-                .get(String.class);
+                .queryParam("datasets", "homo_sapiens_core_60_37e").queryParam("config", "gene")
+                .accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
 
@@ -158,30 +141,32 @@ public class PortalResourceTest extends JerseyTest {
 
         assertTrue(children.isArray());
 
-        for(Iterator<JsonNode> it = children.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = children.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
-            assertPropertiesNotNull(curr, "displayName", "name", "independent",
-                "maxAttributes", "maxContainers");
+            assertPropertiesNotNull(curr, "displayName", "name", "independent", "maxAttributes", "maxContainers");
         }
     }
 
     @Test
     public void testGetMartsForGuiContainer() throws IOException {
         WebResource webResource = resource();
-        String responseMsg = webResource.path("/martservice/marts")
-                .queryParam("guicontainer", "default")
-                .accept("application/json")
-                .get(String.class);
+        try {
+            Thread.sleep(1000);
+            Log.debug("test");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String responseMsg = webResource.path("/martservice/marts").queryParam("guicontainer", "default")
+                .accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
         int count = 0;
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
-            assertPropertiesNotNull(curr, "displayName", "name",
-                    "config", "operation", "meta");
+            assertPropertiesNotNull(curr, "displayName", "name", "config", "operation", "meta");
             count++;
         }
 
@@ -192,18 +177,15 @@ public class PortalResourceTest extends JerseyTest {
     public void testGetFilters() throws IOException {
         WebResource webResource = resource();
         String responseMsg = webResource.path("/martservice/filters")
-                .queryParam("datasets", "homo_sapiens_core_60_37e")
-                .queryParam("config", "gene")
-                .queryParam("container", "gene_filter")
-                .accept("application/json")
-                .get(String.class);
+                .queryParam("datasets", "homo_sapiens_core_60_37e").queryParam("config", "gene")
+                .queryParam("container", "gene_filter").accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
         int count = 0;
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
             assertPropertiesNotNull(curr, "displayName", "name", "isHidden", "type");
             count++;
@@ -216,91 +198,87 @@ public class PortalResourceTest extends JerseyTest {
     public void testGetAttributes() throws IOException {
         WebResource webResource = resource();
         String responseMsg = webResource.path("/martservice/attributes")
-                .queryParam("datasets", "homo_sapiens_core_60_37e")
-                .queryParam("config", "gene")
-                .queryParam("container", "gene_attribute")
-                .accept("application/json")
-                .get(String.class);
+                .queryParam("datasets", "homo_sapiens_core_60_37e").queryParam("config", "gene")
+                .queryParam("container", "gene_attribute").accept("application/json").get(String.class);
         ObjectMapper mapper = new ObjectMapper();
         JsonNode node = mapper.readTree(responseMsg);
         int count = 0;
 
         assertTrue(node.isArray());
 
-        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+        for (Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
             JsonNode curr = it.next();
-            assertPropertiesNotNull(curr, "displayName", "name", "isHidden",
-                    "value", "linkURL");
+            assertPropertiesNotNull(curr, "displayName", "name", "isHidden", "value", "linkURL");
             count++;
         }
 
         assertEquals(14, count);
     }
 
-//    @Test
-//    public void testGetProcessorGroups() throws IOException {
-//        WebResource webResource = resource();
-//        String responseMsg = webResource.path("/martservice/processorgroups")
-//                .queryParam("mart", "gene")
-//                .accept("application/json")
-//                .get(String.class);
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode node = mapper.readTree(responseMsg);
-//
-//        assertTrue(node.isArray());
-//
-//        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
-//            JsonNode curr = it.next();
-//            assertPropertiesNotNull(curr, "displayName", "name");
-//        }
-//    }
-//
-//    @Test
-//    public void testGetProcessors() throws IOException {
-//        WebResource webResource = resource();
-//        String responseMsg = webResource.path("/martservice/processors")
-//                .queryParam("mart", "gene")
-//                .queryParam("processorgroup", "Tabular")
-//                .accept("application/json")
-//                .get(String.class);
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonNode node = mapper.readTree(responseMsg);
-//
-//        assertTrue(node.isArray());
-//
-//        for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
-//            JsonNode curr = it.next();
-//            assertPropertiesNotNull(curr, "displayName", "name");
-//        }
-//    }
-//
-//    @Test
-//    public void testResults() {
-//
-//        String xml = "<!DOCTYPE Query><Query client=\"test\" processor=\"TSV\" limit=\"10\" header=\"1\">"
-//                + "<Dataset name=\"homo_sapiens_core_60_37e\" config=\"gene\">"
-//                + "<Attribute name=\"gene_gene_id_1029\"/></Dataset></Query>";
-//
-//        WebResource webResource = resource();
-//        String responseMsg = webResource.path("/martservice/results")
-//                .queryParam("query", xml)
-//                .accept("text/plain")
-//                .get(String.class);
-//
-//        String expected = "Gene id 1029\n"
-//                + "161049\n"
-//                + "161050\n"
-//                + "161051\n"
-//                + "161052\n"
-//                + "161053\n"
-//                + "161054\n"
-//                + "161055\n"
-//                + "161056\n"
-//                + "161057\n"
-//                + "161058";
-//
-//        System.out.println(responseMsg);
-//
-//        assertEquals(expected, responseMsg);
-//    }
+    // @Test
+    // public void testGetProcessorGroups() throws IOException {
+    // WebResource webResource = resource();
+    // String responseMsg = webResource.path("/martservice/processorgroups")
+    // .queryParam("mart", "gene")
+    // .accept("application/json")
+    // .get(String.class);
+    // ObjectMapper mapper = new ObjectMapper();
+    // JsonNode node = mapper.readTree(responseMsg);
+    //
+    // assertTrue(node.isArray());
+    //
+    // for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+    // JsonNode curr = it.next();
+    // assertPropertiesNotNull(curr, "displayName", "name");
+    // }
+    // }
+    //
+    // @Test
+    // public void testGetProcessors() throws IOException {
+    // WebResource webResource = resource();
+    // String responseMsg = webResource.path("/martservice/processors")
+    // .queryParam("mart", "gene")
+    // .queryParam("processorgroup", "Tabular")
+    // .accept("application/json")
+    // .get(String.class);
+    // ObjectMapper mapper = new ObjectMapper();
+    // JsonNode node = mapper.readTree(responseMsg);
+    //
+    // assertTrue(node.isArray());
+    //
+    // for(Iterator<JsonNode> it = node.iterator(); it.hasNext();) {
+    // JsonNode curr = it.next();
+    // assertPropertiesNotNull(curr, "displayName", "name");
+    // }
+    // }
+    //
+    // @Test
+    // public void testResults() {
+    //
+    // String xml = "<!DOCTYPE Query><Query client=\"test\" processor=\"TSV\" limit=\"10\" header=\"1\">"
+    // + "<Dataset name=\"homo_sapiens_core_60_37e\" config=\"gene\">"
+    // + "<Attribute name=\"gene_gene_id_1029\"/></Dataset></Query>";
+    //
+    // WebResource webResource = resource();
+    // String responseMsg = webResource.path("/martservice/results")
+    // .queryParam("query", xml)
+    // .accept("text/plain")
+    // .get(String.class);
+    //
+    // String expected = "Gene id 1029\n"
+    // + "161049\n"
+    // + "161050\n"
+    // + "161051\n"
+    // + "161052\n"
+    // + "161053\n"
+    // + "161054\n"
+    // + "161055\n"
+    // + "161056\n"
+    // + "161057\n"
+    // + "161058";
+    //
+    // System.out.println(responseMsg);
+    //
+    // assertEquals(expected, responseMsg);
+    // }
 }
