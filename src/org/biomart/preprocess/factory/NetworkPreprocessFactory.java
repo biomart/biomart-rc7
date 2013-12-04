aa5efd421beedc4355e3c241fe9796aba4b4d9f8
Luca Pandini <luca1.pandini@gmail.com>
2013-12-04 10:50:18 +0100

    rename this preprocess and fix missing exception message

--- src/org/biomart/preprocess/factory/NetworkPreprocessFactory.java
+++ src/org/biomart/preprocess/factory/NetworkPreprocessFactory.java
@@ -3,14 +3,14 @@ package org.biomart.preprocess.factory;
 import org.biomart.common.resources.Log;
 import org.biomart.preprocess.Preprocess;
 import org.biomart.preprocess.PreprocessParameters;
-import org.biomart.preprocess.QuerySplit;
+import org.biomart.preprocess.Network;
 
 public class NetworkPreprocessFactory implements PreprocessFactory {
 
 	@Override
 	public Preprocess newPreprocess(PreprocessParameters pp) {
 		Log.debug(NetworkPreprocessFactory.class.getName() + " invoked");
-		return new QuerySplit(pp);
+		return new Network(pp);
 	}
 
 }
