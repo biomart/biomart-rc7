830479cfbede845b238394b18b7d1ea45d49e729
Luca Pandini <luca1.pandini@gmail.com>
2014-02-04 13:40:47 +0100

    rename converter

--- src/org/biomart/dino/converters/Converter.java
+++ src/org/biomart/dino/converters/Converter.java
@@ -15,5 +15,26 @@ public interface Converter {
     
     public Converter setOutput(OutputStream out);
     
+    /**
+     * 
+     * @return The dataset name used within the query for translation.
+     */
+    public String getDatasetName();
+    
+    /**
+     * 
+     * @return The configuration name used within the query for translation.
+     */
+    public String getConfigName();
+    
+    /**
+     * 
+     * It is used a different Attribute based on the translation type:
+     * for annotations, human, mouse etc.
+     * 
+     * @return The Attribute used to understand what translation to do and do it. 
+     */
+    public Attribute getAttributeForTranslation();
+    
     
 }
