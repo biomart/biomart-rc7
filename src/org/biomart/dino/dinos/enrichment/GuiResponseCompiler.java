3f0932ae295e47df243827be69d51b9a234542a4
Luca Pandini <luca1.pandini@gmail.com>
2014-03-07 18:26:22 +0100

    don't use mustache anymore because it removes angular expressions

--- src/org/biomart/dino/dinos/enrichment/GuiResponseCompiler.java
+++ src/org/biomart/dino/dinos/enrichment/GuiResponseCompiler.java
@@ -12,19 +12,28 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+
 public class GuiResponseCompiler {
 
+    public static String ROOT_KEY = "data";
 
+    // The mustache compiler clashes with angular expression format...
+    // I introduced angularjs after have chose to use mustache.
+    // TODO: use another template engine?
     public static void
     compile(File tpl, OutputStream out, Map<String, Object> binding) throws IOException {
         FileReader in = null;
         BufferedWriter writer = null;
         try {
-            in = new FileReader(tpl);
+//            in = new FileReader(tpl);
             writer = new BufferedWriter(new OutputStreamWriter(out));
-            DefaultMustacheFactory mf = new DefaultMustacheFactory();
-            Mustache mustache = mf.compile(in, "enrichment.html", "[", "]");
-            mustache.execute(writer, binding);
+//            DefaultMustacheFactory mf = new DefaultMustacheFactory();
+//            Mustache mustache = mf.compile(in, "enrichment.html", "[", "]");
+//            mustache.execute(writer, binding);
+            String content = FileUtils.readFileToString(tpl);
+            content = content.replaceFirst("@@.*@@", binding.get(ROOT_KEY).toString());
+            writer.append(content);
             writer.flush();
         } finally {
             if (in != null) {
